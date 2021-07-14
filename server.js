7d43917f9c58c818741c43c2e086a3f9de7d5839
Agni-Saha <agnisaha337599@gmail.com>
2021-07-15 02:24:37 +0530

    added env var in place of mongodb url

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-const uri = "mongodb+srv://agnisaha337599:IAMAWESOME.@cluster0.dbq7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
